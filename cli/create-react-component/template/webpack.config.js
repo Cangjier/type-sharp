30020d1a758375dda8b68bf94a17b2e3834bcebd
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-23 08:11:21 +0000

    fix webpack

--- cli/create-react-component/template/webpack.config.js
+++ cli/create-react-component/template/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
     entry: './src/index.tsx',
