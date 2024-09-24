96d3614370769ad457849bdc64aa2a567117fcfe
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-24 15:33:30 +0000

    optimize .tsc

--- cli/.tsc/context.ts
+++ cli/.tsc/context.ts
@@ -6,10 +6,10 @@ export const null: any = 0 as any;
 export const undefined: any = 0 as any;
 export const GetType:()=> Type = 0 as any
 export const ToString:()=> string = 0 as any
-export const Equals:(null)=> boolean = 0 as any
+export const Equals:(obj?: any)=> boolean = 0 as any
 export const GetHashCode:()=> number = 0 as any
-export const exec:(path?: string, args?: string[]) => number = 0 as any
-export const start:(path?: string, args?: string[]) => void = 0 as any
+export const exec:(path?: string, ...args: string[]) => number = 0 as any
+export const start:(path?: string, ...args: string[]) => void = 0 as any
 export const cmd:(workingDirectory?: string, commandLine?: string) => number = 0 as any
 export const startCmd:(workingDirectory?: string, commandLine?: string) => void = 0 as any
 export const parseFloat:(value?: string) => number = 0 as any
