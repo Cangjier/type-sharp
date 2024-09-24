a1c0c5dcad04675f17e430ca3c51c46a0c292372
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-24 03:22:55 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # 自动将Releases下最新版本下载
 # 1. 下载最新版本
-wget https://github.com/Cangjier/type-sharp/releases/download/1.3.20240923.5/tsc
+wget https://github.com/Cangjier/type-sharp/releases/download/1.4.20240924.6/tsc
 # 2. 移动到/usr/local/bin
 sudo mv tsc /usr/local/bin
 # 3. 添加可执行权限
