4e6c72651b591618867420b81cc8262bf6080c45
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-24 09:58:59 +0000

    optimize init

--- cli/init/main.ts
+++ cli/init/main.ts
@@ -1,3 +1,3 @@
 import { exec } from "../.tsc/context";
-import {Environment} from "../.tsc/System/Environment";
-exec(Environment.ProcessPath,["run","cs2ts"]);
+import { Environment } from "../.tsc/System/Environment";
+exec(Environment.ProcessPath, "run", "cs2ts");
