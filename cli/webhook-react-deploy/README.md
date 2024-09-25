60693cff0e34b2d3f3abae9d90a40d937a89a404
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-25 09:04:58 +0000

    optimize webhook

--- cli/webhook-react-deploy/README.md
+++ cli/webhook-react-deploy/README.md
@@ -1,2 +1,3 @@
 # webhook-deploy
 该命令行是在服务器上启动webhook服务，当git push时，自动拉取仓库，进行编译，编译完成后自动部署。
+# 使用教程
--- cli/webhook-react-deploy/main.ts
+++ cli/webhook-react-deploy/main.ts
@@ -1,11 +1,12 @@
-import { Server } from '../.tsc/TypeSharp/System/Server'
-import { Session } from '../.tsc/TidyHPC/Routers/Urls/Session'
-import { args } from '../.tsc/context'
+import { Server } from '../.tsc/TypeSharp/System/Server';
+import { Session } from '../.tsc/TidyHPC/Routers/Urls/Session';
+import { args } from '../.tsc/context';
 
 let main = async () => {
     let server = new Server();
     server.use("/api/v1/webhook", async (session: Session) => {
         console.log(session.Request.Url);
+        console.log(await session.Cache.GetRequstBodyJson());
     });
     await server.start(Number(args[0]));
 };
