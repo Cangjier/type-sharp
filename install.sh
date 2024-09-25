8fca7c418155a28061f72cc68d62c501db7465cc
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-25 15:34:09 +0000

    optimize install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # 自动将Releases下最新版本下载
 # 1. 下载最新版本
-wget --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tsc
+wget --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tscl
 # 2. 移动到/usr/local/bin
-sudo mv tsc /usr/local/bin
+sudo mv tscl /usr/local/bin
 # 3. 添加可执行权限
-sudo chmod +x /usr/local/bin/tsc
+sudo chmod +x /usr/local/bin/tscl
 exit
