b1ff79de06f4c8501f3ea135bba4b8d41ee2d798
李东明 <moodlee@qq.com>
2024-09-26 17:35:17 +0800

    _

--- cli/webhook-react-deploy-service/main.ts
+++ cli/webhook-react-deploy-service/main.ts
@@ -1,6 +1,6 @@
 import { args, cmdAsync } from "../.tsc/context";
 import { Environment } from "../.tsc/System/Environment";
 let port  = args.length > 0 ? args[0] : "8080";
-let cmd = `/usr/local/bin tscl run register-service webhook-react-deploy-${port} "/usr/local/bin tscl run webhook-react-deploy ${port}"`;
+let cmd = `/usr/local/bin/tscl run register-service webhook-react-deploy-${port} "/usr/local/bin/tscl run webhook-react-deploy ${port}"`;
 console.log(cmd);
 await cmdAsync(Environment.CurrentDirectory, cmd);
