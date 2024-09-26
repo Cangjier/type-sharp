d2c331a8ecaf8d2d03ea6244fa907c8f46a49c0f
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-26 04:01:49 +0000

    optimize npm

--- cli/npm/main.ts
+++ cli/npm/main.ts
@@ -3,18 +3,24 @@ import { Environment } from "../.tsc/System/Environment";
 import { axios } from "../.tsc/TypeSharp/System/axios";
 let main = async () => {
     let response = await axios.get("https://mirrors.cloud.tencent.com/nodejs-release/index.json");
-    // wget https://mirrors.cloud.tencent.com/nodejs-release/v18.18.0/node-v18.18.0-linux-x64.tar.xz
-    let versions = JSON.parse(response.data);
-    let ltsVersions = versions.filter((v: any) => v.lts);
-    let latestLtsVersion = ltsVersions[0].version;
-    let downloadUrl = `https://mirrors.cloud.tencent.com/nodejs-release/${latestLtsVersion}/node-${latestLtsVersion}-linux-x64.tar.xz`;
-    console.log(`Downloading ${downloadUrl}`);
-    await cmdAsync(Environment.CurrentDirectory, `wget ${downloadUrl}`);
-    await execAsync(Environment.CurrentDirectory, `tar -xf node-${latestLtsVersion}-linux-x64.tar.xz`);
-    await execAsync(Environment.CurrentDirectory, `sudo mv node-${latestLtsVersion}-linux-x64 /usr/local/nodejs`);
-    await execAsync(Environment.CurrentDirectory, `rm node-${latestLtsVersion}-linux-x64.tar.xz`);
-    await execAsync(Environment.CurrentDirectory, `echo 'export PATH=/usr/local/nodejs/bin:$PATH' >> ~/.bashrc`);
-    await execAsync(Environment.CurrentDirectory, `source ~/.bashrc`);
+    try {
+        let versions = JSON.parse(response.data);
+        let ltsVersions = versions.filter((v: any) => v.lts);
+        let latestLtsVersion = ltsVersions[0].version;
+        console.log(`Latest LTS version is ${latestLtsVersion}`);
+        let downloadUrl = `https://mirrors.cloud.tencent.com/nodejs-release/${latestLtsVersion}/node-${latestLtsVersion}-linux-x64.tar.xz`;
+        console.log(`Downloading ${downloadUrl}`);
+        await cmdAsync(Environment.CurrentDirectory, `wget ${downloadUrl}`);
+        await execAsync(Environment.CurrentDirectory, `tar -xf node-${latestLtsVersion}-linux-x64.tar.xz`);
+        await execAsync(Environment.CurrentDirectory, `sudo mv node-${latestLtsVersion}-linux-x64 /usr/local/nodejs`);
+        await execAsync(Environment.CurrentDirectory, `rm node-${latestLtsVersion}-linux-x64.tar.xz`);
+        await execAsync(Environment.CurrentDirectory, `echo 'export PATH=/usr/local/nodejs/bin:$PATH' >> ~/.bashrc`);
+        await execAsync(Environment.CurrentDirectory, `source ~/.bashrc`);
+    }
+    catch (e) {
+        console.log(response, e);
+        throw e;
+    }
 };
 
 await main();
