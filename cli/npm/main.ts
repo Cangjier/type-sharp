a80f54ec86928b57ec1c91d0d9eb07c69da8bec9
Cangjier <39083672+Cangjier@users.noreply.github.com>
2024-09-26 03:56:47 +0000

    optimize npm

--- cli/npm/main.ts
+++ cli/npm/main.ts
@@ -5,8 +5,16 @@ let main = async () => {
     let response = await axios.get("https://mirrors.cloud.tencent.com/nodejs-release/index.json");
     // wget https://mirrors.cloud.tencent.com/nodejs-release/v18.18.0/node-v18.18.0-linux-x64.tar.xz
     let versions = JSON.parse(response.data);
-    let ltsVersions = versions.filter((v: any) => v.lts!=false);
-    console.log(ltsVersions);
+    let ltsVersions = versions.filter((v: any) => v.lts);
+    let latestLtsVersion = ltsVersions[0].version;
+    let downloadUrl = `https://mirrors.cloud.tencent.com/nodejs-release/${latestLtsVersion}/node-${latestLtsVersion}-linux-x64.tar.xz`;
+    console.log(`Downloading ${downloadUrl}`);
+    await cmdAsync(Environment.CurrentDirectory, `wget ${downloadUrl}`);
+    await execAsync(Environment.CurrentDirectory, `tar -xf node-${latestLtsVersion}-linux-x64.tar.xz`);
+    await execAsync(Environment.CurrentDirectory, `sudo mv node-${latestLtsVersion}-linux-x64 /usr/local/nodejs`);
+    await execAsync(Environment.CurrentDirectory, `rm node-${latestLtsVersion}-linux-x64.tar.xz`);
+    await execAsync(Environment.CurrentDirectory, `echo 'export PATH=/usr/local/nodejs/bin:$PATH' >> ~/.bashrc`);
+    await execAsync(Environment.CurrentDirectory, `source ~/.bashrc`);
 };
 
 await main();
