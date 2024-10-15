a48450057474945d6075531631b579224b544d90
李东明 <moodlee@qq.com>
2024-10-15 17:01:52 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,20 @@ else
     wget --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tscl
 fi
 
-# 3. 找到并杀掉所有tscl进程
-for pid in $(ps -ef | grep tscl | grep -v grep | awk '{print $2}'); do
-    sudo kill -9 $pid
-done
+# 停止所有ExecStart带有tscl的服务
+systemctl list-units --type=service | grep tscl | awk '{print $1}' | xargs -r systemctl stop
+
+# 延迟3秒
+sleep 3
 
 # 4. 移动到/usr/local/bin
 sudo mv tscl /usr/local/bin
 
 # 5. 添加可执行权限
 sudo chmod +x /usr/local/bin/tscl
+
+# 启动所有ExecStart带有tscl的服务
+systemctl list-units --type=service | grep tscl | awk '{print $1}' | xargs -r systemctl start
 
 # 6. 退出
 exit
