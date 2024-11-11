b5e5357aa73da2b1e52913052ab8edfb7cd7df2e
Cangjier <moodlee@qq.com>
2024-11-11 16:22:28 +0800

    optimize

--- install.sh
+++ install.sh
@@ -1,28 +1,36 @@
-# 自动将Releases下最新版本下载
+#!/bin/bash
+
+# 自动下载Releases下的最新版本并更新服务
+
 # 1. 检查是否存在git配置的代理
 proxy=$(git config --get http.proxy)
 
 # 2. 下载最新版本
+download_url="https://github.com/Cangjier/type-sharp/releases/download/latest/tscl"
 if [ -n "$proxy" ]; then
-    # 输出日志
-    echo "use proxy: $proxy"
-    wget -e "https_proxy=$proxy" --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tscl
-    # wget -e "https_proxy=http://127.0.0.1:7897" --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tscl
+    echo "Using proxy: $proxy"
+    wget -e "https_proxy=$proxy" --no-cache "$download_url"
 else
-    wget --no-cache https://github.com/Cangjier/type-sharp/releases/download/latest/tscl
+    wget --no-cache "$download_url"
 fi
 
 echo "Stop all tscl services"
-# 停止所有ExecStart带有tscl的服务
-systemctl list-units --type=service | grep 'tscl run' | awk '{print $1}' | xargs -r systemctl stop
+
+# 停止所有包含 'tscl' 的服务
+for service in $(systemctl list-units --type=service --no-legend --all | awk '{print $1}'); do
+    # 检查该服务的ExecStart是否包含'tscl'
+    if systemctl show "$service" -p ExecStart | grep -q 'tscl'; then
+        sudo systemctl stop "$service"
+    fi
+done
 
 # 延迟3秒
 sleep 3
 
 echo "Kill all tscl processes"
 # 找到并杀掉所有tscl进程
-for pid in $(ps -ef | grep tscl | grep -v grep | awk '{print $2}'); do
-    sudo kill -9 $pid
+for pid in $(pgrep -f tscl); do
+    sudo kill -9 "$pid"
 done
 
 # 延迟1秒
@@ -34,11 +42,16 @@ sudo mv tscl /home/ubuntu/.tscl/bin
 # 5. 添加可执行权限
 sudo chmod +x /home/ubuntu/.tscl/bin/tscl
 
-# 启动所有ExecStart带有tscl的服务
-systemctl list-units --type=service | grep 'tscl run' | awk '{print $1}' | xargs -r systemctl start
+echo "Start all tscl services"
+# 启动所有包含 'tscl' 的服务
+for service in $(systemctl list-units --type=service --no-legend --all | awk '{print $1}'); do
+    if systemctl show "$service" -p ExecStart | grep -q 'tscl'; then
+        sudo systemctl start "$service"
+    fi
+done
 
 # 6. 在.bashrc中添加环境变量，如果不存在则添加
-if [ -z "$(cat ~/.bashrc | grep 'export PATH=$PATH:/home/ubuntu/.tscl/bin')" ]; then
+if ! grep -q 'export PATH=$PATH:/home/ubuntu/.tscl/bin' ~/.bashrc; then
     echo 'export PATH=$PATH:/home/ubuntu/.tscl/bin' >>~/.bashrc
 fi
 
