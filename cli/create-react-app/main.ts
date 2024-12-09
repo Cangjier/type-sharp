38739cafd2c7911a431a4c14a4d3c7392d0f07c6
Cangjier <moodlee@qq.com>
2024-12-09 10:15:54 +0000

    optimize create-react-app

--- cli/create-react-app/main.ts
+++ cli/create-react-app/main.ts
@@ -4,46 +4,74 @@ import { Json } from '../.tsc/TidyHPC/LiteJson/Json';
 import { File } from '../.tsc/System/IO/File';
 import { UTF8Encoding } from '../.tsc/System/Text/UTF8Encoding';
 import { Directory } from '../.tsc/System/IO/Directory';
+import { shell } from "../.tsc/Cangjie/TypeSharp/System/shell";
+import { OperatingSystem } from '../.tsc/System/OperatingSystem';
 
-let main = () => {
+let main = async () => {
     console.log(`create-react-app`);
     let script_directory = Path.GetDirectoryName(script_path);
     let templateDirectory = Path.Combine(script_directory, "template");
     let utf8 = new UTF8Encoding(false);
     let projectDirectory = args.length > 0 ? Path.GetFullPath(args[0]) : Directory.GetCurrentDirectory();
+    let sh = shell.start({
+        filePath: OperatingSystem.IsLinux() ? "bash" : "cmd",
+        workingDirectory: projectDirectory
+    });
     console.log(`working in : ${projectDirectory}`);
     if (Directory.Exists(projectDirectory) == false) {
         Directory.CreateDirectory(projectDirectory);
     }
-    console.log(`npm install create-react-app --global`);
-    if (cmd(projectDirectory, `npm install create-react-app --global`).exitCode != 0) {
-        console.log(`npm install create-react-app --global failed`);
-        return -1;
-    }
-    console.log(`create-react-app . --template typescript`);
-    if (cmd(projectDirectory, `create-react-app . --template typescript`).exitCode != 0) {
-        return -1;
-    }
-    console.log(`npm install antd --save`);
-    if (cmd(projectDirectory, `npm install antd --save`).exitCode != 0) {
-        console.log(`npm install antd --save failed`);
-        return -1;
-    }
+    sh.writeLine(`npm install create-react-app --global & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
+    sh.writeLine(`create-react-app. --template typescript & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
+    sh.writeLine(`npm install antd --save & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
     console.log(`npm install @ant-design/icons --save`);
-    if (cmd(projectDirectory, `npm install @ant-design/icons --save`).exitCode != 0) {
-        console.log(`npm install @ant-design/icons --save failed`);
-        return -1;
-    }
+    sh.writeLine(`npm install @ant-design/icons --save & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
     console.log(`npm install axios --save`);
-    if (cmd(projectDirectory, `npm install axios --save`).exitCode != 0) {
-        console.log(`npm install axios --save failed`);
-        return -1;
-    }
+    sh.writeLine(`npm install axios --save & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
     console.log(`npm install react-router-dom --save`);
-    if (cmd(projectDirectory, `npm install react-router-dom --save`).exitCode != 0) {
-        console.log(`npm install react-router-dom --save failed`);
-        return -1;
-    }
+    sh.writeLine(`npm install react-router-dom --save & echo ---`);
+    await sh.readLinesWhen(item => {
+        if (item == "---") {
+            return true;
+        }
+        console.log(item);
+        return false;
+    });
     let envPath = Path.Combine(projectDirectory, ".ENV");
     File.WriteAllText(envPath, `PUBLIC_URL=${Path.GetFileName(projectDirectory)}`, utf8);
     console.log(`.ENV file created`);
@@ -53,4 +81,4 @@ let main = () => {
     return 0;
 };
 
-main();
+await main();
