b601697f1e3c048421da99ed50703ff8d26ac917
Cangjier <moodlee@qq.com>
2024-12-10 03:00:07 +0000

    optimize

--- cli/create-vite-app/template/main.tsx
+++ cli/create-vite-app/template/main.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+const VITE_PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={VITE_PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App></App>} />
       </Routes>
