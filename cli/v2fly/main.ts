544dd749d74898ae0520a25ea54edc388300c495
Cangjier <moodlee@qq.com>
2025-11-07 10:12:39 +0800

    Update User-Agent header in axios request to 'clash' for improved compatibility

--- cli/v2fly/main.ts
+++ cli/v2fly/main.ts
@@ -240,7 +240,7 @@ let V2flyManager = () => {
         let response = await axios.get(url, {
             responseType: "text",
             headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+                "User-Agent": "clash"
             }
         });
         if (response.status != 200) {
