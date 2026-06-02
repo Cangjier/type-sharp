dc657292d17e5c1d75ce7ebb89ca53f225c1138c
Cangjier <moodlee@qq.com>
2026-06-02 14:51:27 +0800

    Add InjectStyle function to main.tsx in create-vite-app template
    - Introduced the InjectStyle function to dynamically inject CSS styles into the document head, enhancing styling capabilities.
    - Applied default styles for body margin and button focus visibility to improve UI consistency.
    - Maintained existing structure for rendering the App component within a BrowserRouter.

--- cli/create-vite-app/template/main.tsx
+++ cli/create-vite-app/template/main.tsx
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const VITE_PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL;
-
+export const InjectStyle = (value: string) => {
+  const id = `rand_${Math.random().toString(36).substr(2, 9)}`
+  if (document.getElementById(id) === null) {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = id;
+    style.innerHTML = value;
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
+}
+InjectStyle(`
+  body{
+    margin:0;
+  }
+  .ant-btn:not(:disabled):focus-visible {
+    outline: none;
+  }
+`);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
