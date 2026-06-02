66a092e2aca7b2b1bef2789148ebf676763c975b
Cangjier <moodlee@qq.com>
2026-06-02 15:19:30 +0800

    Enhance Vite configuration in create-vite-app template
    - Added build time definition to the Vite config for better tracking of build instances.
    - Improved base URL handling to account for undefined environment variables.
    - Configured output directory based on environment variables, ensuring proper build output management.
    - Added console logs for VITE_OUT_DIR and build time for improved debugging and visibility.

--- cli/create-vite-app/template/vite.config.ts
+++ cli/create-vite-app/template/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+
 const env = loadEnv('production', process.cwd());
 export default defineConfig({
   plugins: [react(), svgr({ svgrOptions: { icon: true } })],
-  base: `${env.VITE_PUBLIC_URL}/`
+  define: {
+    __BUILD_TIME__: (new Date()).getTime(),
+  },
+  base: env.VITE_PUBLIC_URL == undefined ? undefined : `${env.VITE_PUBLIC_URL}/`,
+  build: {
+    outDir: (env.VITE_OUT_DIR == undefined || env.VITE_PUBLIC_URL == undefined) ? undefined : `${env.VITE_OUT_DIR}/${env.VITE_PUBLIC_URL}`,
+    emptyOutDir: true
+  }
 })
 
+console.log(`VITE_OUT_DIR: ${env.VITE_OUT_DIR}`);
 console.log(`VITE_PUBLIC_URL: ${env.VITE_PUBLIC_URL}`);
+console.log(`Time: ${new Date().toISOString()}`);
