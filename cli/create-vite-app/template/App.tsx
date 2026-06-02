72bd21ea3ec3dd9bfa45ef3c6e489035698bba4f
Cangjier <moodlee@qq.com>
2026-06-02 15:40:32 +0800

    Refactor App component to use forwardRef and enhance props in create-vite-app template
    - Updated the App component to utilize forwardRef, allowing for better ref handling.
    - Introduced IAppRef and IAppProps interfaces to define component props and ref types, improving type safety.
    - Enhanced the layout structure with additional styling for better visual presentation.

--- cli/create-vite-app/template/App.tsx
+++ cli/create-vite-app/template/App.tsx
@@ -1,26 +1,46 @@
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 import './App.css'
 
-function App() {
+export interface IAppRef {
+  refresh: () => void;
+}
+export interface IAppProps {
+  style?: React.CSSProperties;
+}
+
+export const App = forwardRef<IAppRef, IAppProps>((props, ref) => {
   return <div style={{
     display: 'flex',
     flexDirection: 'column',
     width: '100vw',
     height: '100vh',
   }}>
-    {/* 顶部 */}
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-    }}>
-      <div></div>
-      <div></div>
+    <div>
+      <div style={{
+        fontSize: '24px',
+        fontWeight: 'bold',
+        color: 'rgba(0, 0, 0, 0.85)',
+        lineHeight: '24px',
+        letterSpacing: '0px',
+        textAlign: 'left',
+        fontStyle: 'normal',
+        fontFamily: 'Source Han Serif SC, sans-serif',
+      }}>
+        {"Demo"}
+      </div>
     </div>
     {/* 中间 */}
-    <div></div>
+    <div>
+      {/* 左侧 */}
+      <div></div>
+      {/* Tab */}
+      <div></div>
+      {/* 右侧 */}
+      <div></div>
+    </div>
     {/* 底部 */}
     <div></div>
   </div>
-}
+});
 
 export default App
